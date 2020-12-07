ddf6ef4d24040c3f18a896c9543c216695192f4f
Leandro <llimas@outlook.com>
2020-12-07 10:46:15 -0300

    icone de maozinha nas categorias

--- resources/js/Pages/Cardapio/CardapioCategoria.vue
+++ resources/js/Pages/Cardapio/CardapioCategoria.vue
@@ -6,21 +6,39 @@
           <div class="font-bold text-xl mb-2">Categorias do Cardápio</div>
         </div>
 
-        <draggable
-          v-model="categorias"
-          @end="onEndDrag"
-        >
+        <draggable v-model="categorias" @end="onEndDrag">
           <transition-group>
             <div
               v-for="categoria in categorias"
               :key="categoria.id"
               class="border-b border-grey-500 cursor-pointer mx-2"
             >
-              <click-edit-input
-                @updated="update(categoria, $event)"
-                @destroied="showModalDeletarItem($event)"
-                :categoria="categoria"
-              ></click-edit-input>
+              <div class="flex items-center hover:bg-gray-100">
+                <div class="handle ml-1 cursor-move w-1/5">
+                  <svg
+                    title="Mover de posição"
+                    class="h-3 md:h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+                    />
+                  </svg>
+                </div>
+                <div class="w-4/5">
+                  <click-edit-input
+                    @updated="update(categoria, $event)"
+                    @destroied="showModalDeletarItem($event)"
+                    :categoria="categoria"
+                  ></click-edit-input>
+                </div>
+              </div>
             </div>
           </transition-group>
         </draggable>
@@ -174,7 +192,7 @@ export default {
     closeModalDeletarItem() {
       this.categoriaSendoDeletada = false;
     },
-    onEndDrag(){
+    onEndDrag() {
       this.$inertia.post(
         "/cardapio/categoria/sort",
         { categorias: this.categorias },
@@ -190,7 +208,7 @@ export default {
           },
         }
       );
-    }
+    },
   },
 
   props: {
